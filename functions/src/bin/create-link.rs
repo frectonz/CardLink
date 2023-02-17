ce5a0fd2ff42340e439ff99b1cd1886166bc1d17
frectonz <fraol0912@gmail.com>
2023-02-17 09:47:05 +0300

    get the username from cognito

--- functions/src/bin/create-link.rs
+++ functions/src/bin/create-link.rs
@@ -9,19 +9,40 @@ struct Link {
     url: String,
 }
 
+// new type pattern wrap `Body` to parse the body as a `Link` type.
+struct LinkBody<'a>(&'a Body);
+
+impl<'a> LinkBody<'a> {
+    fn json(&self) -> Option<Link> {
+        match &self.0 {
+            Body::Empty => None,
+            Body::Binary(_) => None,
+            Body::Text(text) => serde_json::from_str::<Link>(text).ok(),
+        }
+    }
+}
+
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    let link = LinkBody(event.body()).json().unwrap_or(Link {
+        name: "unknown".to_string(),
+        url: "unknown".to_string(),
+    });
+
     let RequestContext::ApiGatewayV1(ctx) = event.request_context();
 
-    let link = event.payload::<Link>()?;
-    let body = serde_json::to_string_pretty(&link).unwrap();
-    let identity = serde_json::to_string_pretty(&ctx.identity).unwrap();
+    let username = ctx
+        .authorizer
+        .get("claims")
+        .and_then(|c| c.get("username"))
+        .and_then(|u| u.as_str())
+        .unwrap_or("anonymous");
 
     Ok(Response::builder()
         .status(200)
         .header("Content-Type", "text/html")
         .body(Body::Text(format!(
-            "Identity = {}\n\nBody = {}",
-            identity, body
+            "Hello, {username}! You are creating a link called {} with URL {}",
+            link.name, link.url,
         )))?)
 }
 
